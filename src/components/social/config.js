f37ce79f2a5cc3603b3470f3ed9beb0acaf37c27
anujeshyadav <114899681+anujeshyadav@users.noreply.github.com>
2023-03-04 19:16:06 +0530

    05/03/2023

---
+++ src/components/social/config.js
@@ -0,0 +1,6 @@
+export default {
+  facebook: "5897811026940152", //Facebook App Id
+  linkedin: "77gjlb9q0zg0o1", //Linkedin app id
+  google:
+    "846832770923-nq1asqrnbgordka380aee3fu0th7kk7l.apps.googleusercontent.com", //Google client id  (7687687687-nq1asqrnbgordksqerer23dewqqe.apps.googleusercontent.com)
+};
